45eb6f0ab6be56135ac55570465e890c13d8ca90
gabriel86ionescu86 <82166695+gabriel86ionescu86@users.noreply.github.com>
2021-04-15 20:30:33 +0300

    Revert "Implemented add book feature"

--- ITSchoolprojectSchoolApp.py
+++ ITSchoolprojectSchoolApp.py
@@ -1,24 +1,5 @@
 def addBook():
-    book_name = input("Insert book name ->")
-    author_name = input("Insert book author ->")
-    #importing csv library
-    import csv
-    with open("booksDB.csv", "w") as file:
-        writer = csv.DictWriter(file,fieldnames=[
-            "BookName", "AuthorName", "SharedWith", "IsRead"
-        ])
-        writer.writerow({"BookName" : book_name,
-                         "AuthorName" : author_name})
-
-    print("Book was successfully added")
-
-
-
-
-
-
-
-
+    print("Add a book option")
 
 def listBooks():
     print("List books option")
@@ -31,7 +12,7 @@ def shareBook():
 
 
 # main menu for user
-print("Menu options: ")
+print("Choose an option: ")
 print("1 : Add a book")
 print("2 : List books")
 print("3 : Update a book")
