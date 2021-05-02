93f88c3c201d84118d01c3da17b46030497bd050
Gabriel <gabriel86ionescu86@gmail.com>
2021-05-02 20:31:01 +0300

    Updated the code and fixed a few bugs: - updated mainMenu to be shown after 1.5 sec after user exits another menu - fixed a bug in addBook and now you can add one/multiple books, and if book exists, is asked to either add new or return to main menu - didn't touch listBooks, works fine - worked on updateBook to detect if book already exists or not, and how to deal with each scenario - I currently have a bug that need to be fixed, editing a book replaces all books in the csv with only the updated book - didn't touch the shareBook feature yet

--- ITSchoolprojectSchoolApp.py
+++ ITSchoolprojectSchoolApp.py
@@ -1,28 +1,32 @@
+# Remember to also delete a certain book
+
 def addBook():
     import csv
-    book_name = input('Insert book name ->')
-    #after getting the book name, we read the csv and compare with the 'BookName' value
-    with open("booksDB.csv", mode="r") as file:
-        rows = csv.DictReader(file, fieldnames=["BookName", "AuthorName", "SharedWith", "IsRead"])
+    book_name = input('Insert book name -> ')
+    with open("booksDB.csv", mode = "r") as file: # we open the csv in read mode and check if the book exists exists
+        rows = csv.DictReader(file,fieldnames=["BookName", "AuthorName", "SharedWith", "IsRead"])
         for row in rows:
-            if row.get('BookName') == book_name:
-                print("The book is already in the list!") #if the same value is found for bookname, we edit to line 23
-                break
-            else:
-                author_name = input('Insert book author ->')
-                with open('booksDB.csv', mode='a', ) as file:  # mode = 'a' appends to the existing list
-                    writer = csv.DictWriter(file, fieldnames=[
-                        'BookName', 'AuthorName', 'SharedWith', 'IsRead'
-                    ])
-                    writer.writerow({'BookName': book_name,
-                                     'AuthorName': author_name,
-                                     'SharedWith': "None",
-                                     'IsRead': False})
-                print('Book was successfully added')
-                break
-    other_book = input('Add another book? Y/N ') #we ask the user if he wants to add another book
-    while other_book == 'Y':
-        addBook() #here we're going again through the compare and add features
+            if row.get("BookName") == book_name: # if the book is found, we ask user to try adding another book
+                    try_again = input(f"{book_name} already exists! Try again? (Y/N? ")
+                    if  try_again.upper() == "Y":
+                        addBook()
+                        return
+                    else:
+                        return
+
+    author_name = input('Insert book author -> ') # if the book is not found, we continue to add it to the list
+    with open('booksDB.csv', mode='a', ) as file:  # mode = 'a' appends to the existing list
+        writer = csv.DictWriter(file, fieldnames=["BookName", "AuthorName", "SharedWith", "IsRead"])
+        writer.writerow({'BookName': book_name,
+                         'AuthorName': author_name,
+                         'SharedWith': "None",
+                         'IsRead': False})
+    print('Book was successfully added!')
+
+    other_book = input('Add another book? (Y/N)? ') # we ask the user if he wants to add another book
+    if other_book.upper() == "Y":
+        addBook()
+    elif other_book.upper() == "N":
         return
 
 
@@ -31,40 +35,83 @@ def listBooks():
     with open("booksDB.csv", mode = "r") as file:
         rows = csv.DictReader(file,fieldnames=["BookName", "AuthorName", "SharedWith", "IsRead"])
         for row in rows:
-            print(f"Book name: {row.get('BookName')}, Author: {row.get('AuthorName')}, Shared with: {row.get('ShareWith')}, Is read: {row.get('IsRead',False)}.")
+            print(f"Book name: {row.get('BookName')}, Author: {row.get('AuthorName')}, Shared with: {row.get('ShareWith')}, Is read: {row.get('IsRead')}.")
 
 
-def updateBook(): # didn't touch this function yet
+def updateBook(): # work in progress, currently the entire csv is removed and only the edited book is kept,can't find a solution yet
     book_name = input("Enter book name: ")
-    book_read = input("Is the book read? (Y/N)? ")
-    if book_read == "Y":
-        book_read = True
-    else:
-        book_read = False
-    rows = []
     import csv
-    with open("booksDB.csv", mode = "a") as file:
-        rows = csv.DictReader(file,fieldnames=[
-            "BookName", "AuthorName", "SharedWith", "IsRead"
-        ])
+    rows_list = []
+    with open('booksDB.csv', mode='r') as file:
+        rows = list(csv.DictReader(file, fieldnames=("BookName", "AuthorName", "SharedWith", "IsRead")))
         for row in rows:
-            if book_name == row.get("BookName"):
+            rows_list.append(row["BookName"]) # we store every book name in a list
+        if book_name not in rows_list: #we search the book the user typed in our list
+            add_new_book = input(f' The {book_name} book does not exits. Would you like to add it? (Y/N)? ')
+            if add_new_book.upper() == "N":
+                return
+            else:
+                addBook()
+                return
+        else:
+            book_read = input("Is the book read? (Y/N)? ")
+            if book_read.upper() == 'Y':
+                book_read = True
+            else:
+                book_read = False
+            rows = []
+    with open('booksDB.csv', mode='r') as file:
+        rows = list(csv.DictReader(file, fieldnames=("BookName", "AuthorName", "SharedWith", "IsRead")))
+        for row in rows:
+            if row["BookName"] == book_name:
                 row["IsRead"] = book_read
-                csv_writer = csv.DictWriter(file, fieldnames=[
-                    "BookName", "AuthorName", "SharedWith", "IsRead"
-                ])
-                csv_writer.writerow(rows)
                 break
+        with open('booksDB.csv', mode='w') as file: # WIP, here I can't make the csv keep all books in the list when editing a certain one
+            csv_writer = csv.DictWriter(file, fieldnames=[
+                "BookName", "AuthorName", "SharedWith", "IsRead"
+            ])
+            csv_writer.writerow({"BookName": row.get("BookName"),
+                                 "AuthorName": row.get("AuthorName"),
+                                 "SharedWith": row.get("SharedWith"),
+                                 "IsRead": row.get("IsRead")}
+                                )
+            # if row["IsRead"] == book_read:
+            #     csv_writer.writerow({"BookName": row.get("BookName"),
+            #                          "AuthorName": row.get("AuthorName"),
+            #                          "SharedWith": row.get("SharedWith"),
+            #                          "IsRead": book_read})
+            print("Book was updated successfully")
 
-        print("Book was updated successfully!")
 
 
-def shareBook(): # didn't touch this function yet
-    print("Share a book option")
 
 
-def mainMenu(): #here we're adding all the options in a list, and attach avaraible "index" to them
+def shareBook(): #share book with smbdy
+    book_name = input("What is the name of the book you want to share?  ")
+    shared_with = input("With whom do you want to share? : ")# tema faceti lista sa contina mai multe persoane
+    import csv
+    with open('booksDB.csv', mode='r') as file:
+        rows = list(csv.DictReader(file, fieldnames=("BookName", "AuthorName", "SharedWith", "IsRead")))
+        for row in rows:
+            if row["BookName"] == book_name:
+                row["SharedWith"] = shared_with
+            else:
+                print("The book is not in database")
+        with open('booksDB.csv', mode='w') as file:
+            csv_writer = csv.DictWriter(file, fieldnames=[
+                "BookName", "AuthorName", "SharedWith", "IsRead"
+            ])
+            csv_writer.writerow({"BookName": row.get("BookName"),
+                                 "AuthorName": row.get("AuthorName"),
+                                 "SharedWith": shared_with,
+                                 "IsRead": row.get("IsRead")}
+                                )
+        print(f"Book was successfully shared with {shared_with}")
+
+
+def mainMenu(): #here we're adding all the options in a list, and attach a variable "index" to them
     # main menu for user
+    import time #we use the time module to add a timeout before returning to main menu
     options = ["Add a book", "List books", "Update a book", "Share a book", "Clear the list", "Quit"]
     index = 1
     print("Select an option from the Main menu")
@@ -76,44 +123,47 @@ def mainMenu(): #here we're adding all the options in a list, and attach avaraib
 
     if option == "1":# starting with this if, we're going to the corresponding function for each attribute
         addBook()
-        quitQuestioning() #here we're calling a function written below that asks user if he want's to exits the program or not
+        time.sleep(1.5)
+        mainMenu()
     elif option == "2":
         listBooks()
-        quitQuestioning()
+        time.sleep(1.5)
+        mainMenu()
     elif option == "3":
         updateBook()
-        quitQuestioning()
+        time.sleep(1.5)
+        mainMenu()
     elif option == "4":
         shareBook()
-        quitQuestioning()
+        time.sleep(1.5)
+        mainMenu()
     elif option == "5":
         clearCSV()
-        quitQuestioning()
+        time.sleep(1.5)
+        mainMenu()
     elif option == "6":
-        print("The app is now closed.")
+        confirmation = input("Are you sure? (Y/N)? ")
+        if confirmation.upper() == "Y":
+            print("The app is now closed.")
+        else:
+            time.sleep(1.5)
+            mainMenu()
     else:
         print("Your option is invalid")
-        quitQuestioning()
+        time.sleep(1.5)
+        mainMenu()
 
 
 def clearCSV():
     # writing a function that clears the list, with confirmation
-    confirmation = input("Are you sure? Y/N ")
-    if confirmation == "Y":
+    confirmation = input("Are you sure? (Y/N)? ")
+    if confirmation.upper() == "Y":
         file = open('booksDB.csv', "w")
         file.close()
-        print('List succesfully deleted.')
+        print('List successfully deleted.')
     else:
         print('Your list is safe. Nothing was removed.')
 
-
-def quitQuestioning():
-    # a function that asks the user if he wants to quit or not after he's done with each option from the menu
-    quit = input("Would you like to quit the app? Y/N ")
-    while quit == 'N':
-        mainMenu()
-        return
-    print("The app is now closed.")
 
 
 mainMenu()
